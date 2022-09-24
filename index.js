e3380b1dafbcd1dd1422e2f800a2e8d352389b60
kumasumit <kumarsumit1989@gmail.com>
2022-09-24 08:31:21 +0530

    Instal express nodemon and fired express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Sumit!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
