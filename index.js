0d823a6242413b3bfda382f05271d508154c5754
kumasumit <kumarsumit1989@gmail.com>
2022-09-24 08:50:37 +0530

    Add controller and route for / home routes

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
-const router = require('./routes')
 const app = express()
 const port = 8000
 
 
-app.get('/', (req, res) => {
-    res.send('Hello Sumit!')
-  })
+
 //this tells the index/root that all routes will be handled by index.js files in routes folder
-router.use('/', require('./routes'));
+app.use('/', require('./routes'));
+//this tells the app to fetch all the routes from index file in routes folder
+//this acts as a middleware for routes
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-
+//include the home_controller
+const homeController = require('../controllers/home_controller');
+router.get('/', homeController.home);
 
 
 module.exports = router
