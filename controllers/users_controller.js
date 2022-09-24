c2f9644f4bbc0f1122bdaf0f75eb00906eff2719
kumasumit <kumarsumit1989@gmail.com>
2022-09-24 16:38:38 +0530

    Create controllers and routes for sign-in and sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,14 @@ module.exports.profile = function(req, res){
     //here user_profile is the user_profile.ejs page in views and title is the context with value User-Profile
 
 }
+//Action 2 for /users/sign-in
+//this renders the sign in page
+module.exports.signIn = function(req, res){
+    res.render('user_sign_in', {title:"Sign In"})
+}
+
+//Action 3 for /users/sign-up
+//this renders the sign up page
+module.exports.signUp = function(req, res){
+    res.render('user_sign_up', {title:"Sign Up"})
+}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 //include the home_controller
 const usersController = require('../controllers/users_controller');
 router.get('/profile', usersController.profile);
-
+router.get('/sign-in', usersController.signIn);
+router.get('/sign-up', usersController.signUp);
 
 module.exports = router
