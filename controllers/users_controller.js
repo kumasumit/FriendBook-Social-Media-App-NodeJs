44a73bbb2feea65d5983c0375eae72013b6eccd2
kumasumit <kumarsumit1989@gmail.com>
2022-09-24 17:37:19 +0530

    Set Up controllers and routes for creating a user/ sign-up form submission

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/users');
 //Action 1 for /users/profile
 module.exports.profile = function(req, res){
     res.render('user_profile', {title:"User-Profile"})
@@ -15,4 +16,38 @@ module.exports.signIn = function(req, res){
 //this renders the sign up page
 module.exports.signUp = function(req, res){
     res.render('user_sign_up', {title:"Sign Up"})
+}
+
+//Action 4 for /users/create to  create a new user
+//this action handles sign_up form submission data
+module.exports.create = function(req, res){
+
+    console.log(req.body);
+    // first check whether password and confirm password are equal or notEqual, if they are not equal send the user back to the sign up page
+    if(req.body.password !== req.body.confirm_password){
+        return res.redirect('back');
+    }
+    //if password and confirm_password are same, search the user corresponding to the email provided in the form/body
+    User.findOne({email:req.body.email}, function(err, user){
+        if(err){
+            console.log('error in signing up');
+            return;
+        }
+        if(!user){
+            //if no user is found for the corresponding email
+            //means no previous user is associated with that email, create and store the new user
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('error in creating user while signing up');
+                    return;
+                }
+                //after creating the user redirect the user to sign in page for the new user to sign in
+                return res.redirect('sign-in');
+            })
+        }
+        // if the user is already present send the control back to sign up page
+        else{
+            return res.redirect('back');
+        }
+    })
 }
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,6 @@ router.get('/profile', usersController.profile);
 router.get('/sign-in', usersController.signIn);
 router.get('/sign-up', usersController.signUp);
 
+router.post('/create', usersController.create);
+
 module.exports = router
