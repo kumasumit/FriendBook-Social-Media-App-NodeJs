9a51924933c92556ea94d597900c8124b5379b52
kumasumit <kumarsumit1989@gmail.com>
2022-09-24 11:26:06 +0530

    Install and Set up ExpressEjsLayouts

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts');
 const app = express()
 const port = 8000
 
+
+app.use(express.static('./assets'))
+app.use(expressLayouts);
+// for getting static files like images, js files and css files
+//we tell the app to look for images, css and script js files in assests folder inside root
+app.set('layout extractStyles',true);
+//here we extract the css, styles file
+app.set('layout extractScripts',true)
+//here we extract the js files
 app.set('view engine', 'ejs');
 //this will set ejs as the view engine
 app.set('views', './views');
 // this will set views to look for views in the views folder
+//this tells the app to use expressLayouts,
+//this must be above app.use routes
+
+
 
 //this tells the index/root that all routes will be handled by index.js files in routes folder
 app.use('/', require('./routes'));
