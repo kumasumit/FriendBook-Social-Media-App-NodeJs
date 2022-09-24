d14b4f44a7e332202dadb85c6374bf5e0a00abc8
kumasumit <kumarsumit1989@gmail.com>
2022-09-24 08:42:35 +0530

    Add and configure express Router

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const router = require('./routes')
 const app = express()
 const port = 8000
 
@@ -6,6 +7,8 @@ const port = 8000
 app.get('/', (req, res) => {
     res.send('Hello Sumit!')
   })
+//this tells the index/root that all routes will be handled by index.js files in routes folder
+router.use('/', require('./routes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
