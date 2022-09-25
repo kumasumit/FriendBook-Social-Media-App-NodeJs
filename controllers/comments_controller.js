7267688309c2314e13feb6a487d1eee14a5027cc
kumasumit <kumarsumit1989@gmail.com>
2022-09-25 19:21:32 +0530

    create controllers and routes for deleting a comment posted by a logged-in user

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -28,3 +28,28 @@ module.exports.create = function(req, res){
 }
 
 //Action 2 to delete a comment, if a signed-in user owns the comment
+//delete comments
+module.exports.destroy = function(req, res)
+{ //find a comment by id
+  Comment.findById(req.params.id, function(err, comment)
+  {
+    //here we are going inside comment models and finding the comment
+    //which the user clicked to delete
+    if(comment.user == req.user.id){
+        //if the user who posted that comment is same as the user trying to delete the comment
+        let postId = comment.post;
+        // comment.post holds the post._id of the post on which the comment is posted
+        comment.remove();
+        //remove/delete the comment
+        Post.findByIdAndUpdate(postId, {$pull:{comments: req.params.id}}, function(err, post){
+            return res.redirect('back');
+        })
+        //go in Posts Schema search the post by id, inside comments array of that post, pull a specific comment by id passed by user and delete it
+        //this will delete the comment_id from the comments array of that particular post
+    }else{
+        //if the user trying to delete the comment is different from user who posted that comment
+        //send the control back to the user
+        return res.redirect('back');
+    }
+  })
+}
--- routes/comments.js
+++ routes/comments.js
@@ -6,4 +6,6 @@ const commentsController = require('../controllers/comments_controller');
 //we use passport.checkAuthentication to check whether user is logged-in or not, so that only logged-in user can create a post
 router.post('/create', passport.checkAuthentication,
 commentsController.create);
+//a router to delete a specfic comment, only if the user is signed in
+router.get('/destroy/:id', passport.checkAuthentication,commentsController.destroy);
 module.exports = router
