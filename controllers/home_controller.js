0274f1389b8fedbfeaf8bb02589fe4e5c461f524
kumasumit <kumarsumit1989@gmail.com>
2022-09-25 14:02:38 +0530

    Create controllers and routes to display a post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,18 @@
 //Action 1 for / route
+const Post = require('../models/posts');
 module.exports.home = function(req, res){
-    return res.render('home', {title: "Home"});
-    //here home is the home.ejs files in views, title is the context with value Home passed to home.ejs file
+
+    Post.find({}).populate('user', 'name email').exec(function(err, posts)
+    //find({}) finds all the posts with specific filter
+    {
+        //here user is populated with name,email and we dont include password for safety
+        if (err) return handleError(err);
+        return res.render('home', {
+            title:"Home",
+            posts: posts
+            //here posts.user contains complete details of user,
+            //after populating the user with id stored in postsSchema
+            //it includes only name, email and not password
+        });
+    })
 }
