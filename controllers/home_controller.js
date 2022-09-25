180cf9633166dcf91c362a5dae00777de93c9023
kumasumit <kumarsumit1989@gmail.com>
2022-09-25 20:18:12 +0530

    create separate posts and user_friends section in home.ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
-//Action 1 for / route
+const User = require('../models/users');
 const Post = require('../models/posts');
+//Action 1 for / route
 module.exports.home = function (req, res) {
 
     // 2nd step for loading post and comment both
@@ -10,8 +11,10 @@ module.exports.home = function (req, res) {
         .populate({
             path: 'comments',
             //populate comments array for each post
+            // this will populate all the comments for that post
             populate: {
                 //populate user inside comment for each comment inside comments array
+                // this will populate the user which created that comment
                 path: 'user'
             }
         })
@@ -19,9 +22,13 @@ module.exports.home = function (req, res) {
             if(err) {
                 return handleError(err);
             }
-            return res.render('home', {
-                title: "Home",
-                posts: posts,
+            User.find({}, function(err, users){
+                //User.find({}) will find all the users from the database
+                    return res.render('home', {
+                        title:" FriendBook || Home",
+                        posts: posts,
+                        all_users: users
+                    });
             })
         })
 }
