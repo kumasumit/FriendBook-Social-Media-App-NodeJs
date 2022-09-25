efd17d66c2dd24bbacb072bea100e1156a517bab
kumasumit <kumarsumit1989@gmail.com>
2022-09-25 14:32:57 +0530

    Create a schema for Comments and link comments_ids for each post in post-Schema

---
+++ models/comments.js
@@ -0,0 +1,27 @@
+//require mongoose
+const mongoose = require('mongoose');
+//design a mongoose Comment Schema
+const commentSchema = new mongoose.Schema({
+    content:{
+        type: String,
+        required: true
+    },
+    //here content is the content of the comment user creates
+
+    user: {
+        //link the comment to the user who created the comment
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    post: {
+        //link the comment to the post on which the comment was posted
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+}, {
+    timestamps: true
+    // we have used timestamps to store createdAt and updatedAt
+})
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
--- models/posts.js
+++ models/posts.js
@@ -8,7 +8,19 @@ const postSchema = new mongoose.Schema({
         type:  mongoose.Schema.Types.ObjectId,
         ref: 'User'
         // here we are using user.id to referece the user schema
-    }
+    },
+
+     comments: [
+        //include the array of ids of all comments in the post Schema itself
+        //this includes the id of all comments associated with a particular post
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Comment'
+        }
+        //here we are referencing the Comment model by comment_id
+        ,
+    ]
+
 },{
     timestamps: true
     // we have used timestamps to store createdAt and updatedAt
