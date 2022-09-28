e2a4bc4ecd1de249be37d6fe55ff890ae148b8fe
kumasumit <kumarsumit1989@gmail.com>
2022-09-28 12:17:43 +0530

    Add likes controller to handle like create/delete request

---
+++ controllers/likes controller.js
@@ -0,0 +1,66 @@
+//import/require Like, Post and Comment Schema
+const Like = require("../models/like");
+const Post =  require("../models/post");
+const Comment = require('../models/comment');
+
+module.exports.toggleLike = async function(req, res){
+    try{
+
+        // likes/toggle/?id=abcdef&type=Post
+        //this is how the route will lok like
+        let likeable;
+        //here likeable is the object on which the likes are placed
+        let deleted = false;
+        //deleted is a boolean to detect whether it is a like or dislike
+        //whether to add the like or delete the like
+        if (req.query.type == 'Post'){
+            //if the like is on a Post, find the post by id and populate the likes array of the post
+            likeable = await Post.findById(req.query.id).populate('likes');
+        }else{
+            //if the like is on a Comment, find the comment by id and populate the likes array of the comment
+            likeable = await Comment.findById(req.query.id).populate('likes');
+        }
+
+        // check if a like already exists
+        let existingLike = await Like.findOne({
+            likeable: req.query.id,
+            onModel: req.query.type,
+            user: req.user._id
+        })
+
+        // if a like already exists then delete it
+        if (existingLike){
+            //delete the like._id from likeable likes array
+            likeable.likes.pull(existingLike._id);
+            likeable.save();
+            existingLike.remove();
+            deleted = true;
+
+        }else{
+            // else make a new like
+            //add the like._id to likeable likes array
+            let newLike = await Like.create({
+                user: req.user._id,
+                likeable: req.query.id,
+                onModel: req.query.type
+            });
+            //
+            likeable.likes.push(newLike._id);
+            likeable.save();
+
+        }
+
+        return res.json(200, {
+            message: "Like Request successful!",
+            data: {
+                deleted: deleted
+            }
+        })
+
+    }catch(err){
+        console.log(err);
+        return res.json(500, {
+            message: 'Internal Server Error'
+        });
+    }
+}
